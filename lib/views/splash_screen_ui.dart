eb9f9a2ebf02aa33455ea61cdf7090a1b85748f9
s6619410036 <s6619410036@sau.ac.th>
2026-01-28 13:51:08 +0700

    commit02: สร้างไฟล์ UI และตั้งค่าหน้าจอแรก

---
+++ lib/views/splash_screen_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenUi extends StatefulWidget {
+  const SplashScreenUi({super.key});
+
+  @override
+  State<SplashScreenUi> createState() => _SplashScreenUiState();
+}
+
+class _SplashScreenUiState extends State<SplashScreenUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFAB80BF),
+    );
+  }
+}
